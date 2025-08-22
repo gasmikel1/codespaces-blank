af6147ffb7fc1323c2f82b1618cd328aebf13974
gasmikel1 <mgaskins845@boyslatin.net>
2025-08-22 15:09:31 +0000

    PyReview

---
+++ unit 1/dataTypeReview.py
@@ -0,0 +1,12 @@
+print('mikel')
+print('Hello world!')
+print(2 > 1)
+print(13)
+x=3233
+y=2643
+print(x*y)
+
+print(10.53)
+
+food = ["food1,food2,food3"]
+print([True,False,10,100,3.14159265,"apple lover", "4 lover"])
