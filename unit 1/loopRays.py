ec9b928362036da64ad6e0c4eb69510efd401437
gasmikel1 <mgaskins845@boyslatin.net>
2025-09-05 15:14:36 +0000

    newChangesOldWorkBy1Day

--- unit 1/loopRays.py
+++ unit 1/loopRays.py
@@ -1,9 +1,15 @@
-numberList= [10980,2998,3,48,57,60,70,89,90]
+numberList= [10,11,45,8,39,48,57,60,70,89,90]
 value=48
 def number(numberList, value):
         for x in numberList:
             print(x)
-            if x == 48:
+            if x == 90:
                  break
 
-number(numberList,value)
+#number(numberList,value)
+
+unorderList= [23,600,4,92,22,49]
+
+
+
+print(unorderList[3:4])
