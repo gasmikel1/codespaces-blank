bc56ff7dc077b1652d5363d640a68985ab4a0e3b
gasmikel1 <mgaskins845@boyslatin.net>
2025-09-17 15:16:27 +0000

    PhoneClassWork

--- unit 1/objects.py
+++ unit 1/objects.py
@@ -1,65 +1,87 @@
 #object is a construct for contaning data and functions.
 
 
-class pro:
-    def __init__(self,username, profileImg, pw):
-        self.username = username
-        self.email = email
-        self.profileImg =profileImg
-        self. pw=pw
-        self. bio=bio
-    def printinf(self.username,):
+#class pro:
+ #   def __init__(self,username, profileImg, pw):
+ #       self.username = username
+ #       self.email = email
+#        self.profileImg =profileImg
+#        self. pw=pw
+ #       self. bio=bio
+ #   def printinf(self.username,):
 
-    def resetPw(self):
-        print('2-step off...')
+  #  def resetPw(self):
+ #       print('2-step off...')
 
-    def uploadImg():
-        print("instructions")
+ #   def uploadImg():
+ #       print("instructions")
 
-    def viewFollowers():
-        print(['list of other followers'])
+ #   def viewFollowers():
+#        print(['list of other followers'])
 
-    profile =pro.username('lekim','IDK','pw','bio' )
+#    profile =pro.username('lekim','IDK','pw','bio' )
 
-print(pro.name)
+#print(pro.name)
 
 #pro.printinf()
 
 
 
 
-class CarMaker:
-    def __init__(self,name,color,year,saeting,model):
+#class CarMaker:
+ #   def __init__(self,name,color,year,saeting,model):
+ #       self.name=name
+  #      self.color=color
+  #      self.saeting=saeting,
+  #      self.year=year
+ #       self.model=model
+ #       self.miles=miles
+        
+ #       def windWipers(self):
+ #           print(' when raining')
+ #       def turnSigh(left,right):
+  #          if left:
+  #              print("turn left")
+ #           elif right:
+ #               print("right")
+   #         else:
+  #              print("no")
+
+ #       def carfaps(self):
+ #           print('name'+name)
+ #           print('name'+year)
+#            print('name'+model)
+
+
+#        def airBag(self):
+#            print('you wont fly out the windsheild')
+
+ #       def bluetooth(year):
+ #               if year>2020:
+  #                  print("bluetooth on")
+ #               else:
+#                    print("you to old!")
+
+#SuperHydrylicInstantTransporter = CarMaker('SuperHydrylicInstantTransporter ','black','5',1230,'-9543 miles')
+#CarMaker()
+
+# obfects is njust a blue print so yeah thats it. I cant see. 
+
+#Oh forgot to indentate but the the primus is the simpal enough. 
+
+#incomplet
+
+
+class phone:
+    def __init__(self,name,call):
         self.name=name
-        self.color=color
-        self.saeting=saeting,
-        self.year=year
-        self.model=model
-        self.miles=miles
-        def windWipers(self):
-            print(' when raining')
-        def turnSigh(feft,right):
-            if left:
-                print("turn left")
-            elif right:
-                print("right")
-            else:
-                print("no")
+        self.call=call
+    
+p1 = phone('ring',"1243141")
 
-        def carfaps(self):
-            print('name'+name)
-            print('name'+year)
-            print('name'+model)
+print(p1.call)
+print(p1.name)
+    
 
-
-        def airBag(self):
-            print('you wont fly out the windsheild')
-
-        def bluetooth(year):
-                if year>2020:
-                    print("bluetooth on")
-                else:
-                    print("you to old!")
-
-SuperHydrylicInstantTransporter = CarMaker('SuperHydrylicInstantTransporter ','black','5',1230,'-9543 miles')
+        
 
