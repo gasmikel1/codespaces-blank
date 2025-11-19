a26593b6aacf0d0ceeb794f62b340937f191fd2c
gasmikel1 <mgaskins845@boyslatin.net>
2025-11-19 15:34:53 +0000

    questionsUpdate

---
+++ unit 2/notes.py
@@ -0,0 +1,8 @@
+"Clarity question is easy to understand"
+
+"Foucsed dont make it broard"
+
+"measurable ask a question that can be answered with data"
+
+"insightful proveds new information"
+
---
+++ unit3/reasrchreveiw.py
@@ -0,0 +1,6 @@
+"does diffeicens turn over contribut to teams win %"
+
+"who has the most passing yard per team" 
+
+from helper3 import weeklyPlayerStats,plot_weekly_player_stats,plot_player_stat
+import matplotlib.pyplot as plt
