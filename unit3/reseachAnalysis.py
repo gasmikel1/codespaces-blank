96dcdb0965cc3b3a82bde8e0203440563ed72bd4
gasmikel1 <mgaskins845@boyslatin.net>
2025-11-24 16:00:26 +0000

    new_update

--- unit3/reseachAnalysis.py
+++ unit3/reseachAnalysis.py
@@ -7,7 +7,7 @@ print(teamsData)
 teameRes = get_season_Results_By_team(2024,'PHI')
 print(teameRes)
 
-#"2 J.Willams had the most  targets"
+#"2 J.Willams had the most  targets" this is also a comparison because we are comparing the number of targets for each player to see who had the most.
 
  
-# "3yesthe time of possesion is strongly correlated with winning as teams that win tend to have higher time of possesion percentages."
+# "3yesthe time of possesion is strongly correlated with winning as teams that win tend to have higher time of possesion percentages."This is a comperison because we are looking at the relationship between time of possesion and winning games.
