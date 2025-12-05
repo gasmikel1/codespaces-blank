0a642574333d07c1c3c3c31f368d97bd6cb0cf7b
gasmikel1 <mgaskins845@boyslatin.net>
2025-12-05 16:16:20 +0000

    washed

---
+++ washed/washed.py
@@ -0,0 +1,17 @@
+from helper import get_season_totals_by_position,plot_position_stat_bar,plot_player_stat_by_week,get_player_stats
+# lost there tuch not in prime decline
+
+stats=get_player_stats(2024,'Deshaun', 'Watson')
+print(stats)
+plot_player_stat_by_week(2024,'Deshaun', 'Watson', 'passing_yards',save_path=' Deshaun_Watson_passing_yard_2024.png')
+plot_player_stat_by_week(2024,'Deshaun', 'Watson', 'rushing_yards',save_path=' Deshaun_Watson_rushing_yard_2024.png')
+plot_player_stat_by_week(2022,'Deshaun', 'Watson', 'rushing_yards',save_path=' Deshaun_Watson_rushing_yard_2024.png')
+plot_player_stat_by_week(2022,'Deshaun', 'Watson', 'passing_yards',save_path=' Deshaun_Watson_passing_yard_2024.png')
+'1 game played shows how healthy they are and availabbilty to play '
+'rushing_yards- indecates ability to run the ball when needed '
+'TD-if cant throw TDs then washed'
+'pits completion %-ability to complete passes'
+
+'bro is not just washed he is super washed even copilot is like bruh. His passing yards are trash and rushing yards are meh for a QB. He is definitely in decline phase of his career.'
+' yeah he had a good year in 2022 but since then its been downhill. He needs to step up his game or else he might be out of the league soon. '
+
