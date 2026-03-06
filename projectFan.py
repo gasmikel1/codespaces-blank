5e83f9a01c64e4188865a9b6e36ae30665efedb2
gasmikel1 <mgaskins845@boyslatin.net>
2026-03-06 16:00:47 +0000

    project_FanV2

--- projectFan.py
+++ projectFan.py
@@ -1,20 +1,28 @@
-def fan(temperature):
+class Fan:
+    
+    temp_thresholds = [20, 25, 30, 35, 40]
+    speeds = [0, 1, 2, 3, 4]
 
-        print("temperature:x")
-        x = input()
-        print("Temperature is: ", x)
-        while True:
-                if temperature > 80:
-                    print("Fan is running at high speed")
-                elif temperature > 70:
-                    print("Fan is running at medium speed")
-                elif temperature > 60:
-                    print("Fan is running at low speed")
-                else:
-                    print("Fan is off")
-                break
-fan(50)
-#note to self use break!
+    def __init__(self, temp):
+        self.current_temp = temp  
 
+    def get_speed(self):
+        
+        if self.current_temp < self.temp_thresholds[0]:
+            return self.speeds[0]
+            
+        for i in range(len(self.temp_thresholds)):
+            
+            if self.current_temp < self.temp_thresholds[i]:
+                return self.speeds[i-1]
+        
+       
+        return self.speeds[-1]
 
-
+# User Input
+try:
+    temp_input = int(input("Enter the temperature: "))
+    my_fan = Fan(temp_input)
+    print("The speed of the fan is:", my_fan.get_speed())
+except ValueError:
+    print("Please enter a valid integer.")
